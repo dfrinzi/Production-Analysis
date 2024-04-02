1caf33b77e54a23276d33c36316009a8565512bd
Dominic Frinzi <dominic.frinzi@basinprecision.com>
2024-04-01 19:22:58 -0500

    finished gross margin import, keeping most recent margin based on highest invoice number

--- gross_margin_reader.py
+++ gross_margin_reader.py
@@ -2,6 +2,7 @@ import pandas as pd
 import settings as s
 
 
+# returns a df with only the most recent (highest invoice #) gross margin data for each part
 def read_gross_margin(df):
     df.rename(columns={'Part No': s.column_part_number,
                        'Invoice No': s.column_invoice_num,
@@ -12,10 +13,20 @@ def read_gross_margin(df):
 
     df.loc[:, s.column_invoice_num] = pd.to_numeric(df.loc[:, s.column_invoice_num], errors="coerce")
     df = df[df[s.column_invoice_num].notna()]
+    unique_parts = df[s.column_part_number].unique()
+    # print(unique_parts)
 
-    df = df.groupby(s.column_part_number)
+    latest_invoice_rows = []
 
-    # df = df.drop_duplicates(s.column_part_number, keep=s.column_invoice_num.max())
+    for part in unique_parts:
+        part_df = df.loc[df[s.column_part_number] == part]
+        row_num = part_df[s.column_invoice_num].idxmax(skipna=True)
+        single_row_df = part_df.loc[[row_num], :]
+        latest_invoice_rows.append(single_row_df)
 
-    # df.reset_index(drop=True, inplace=True)
-    print(df)
+    gm_latest_invoice_rows_df = pd.concat(latest_invoice_rows, axis=0, ignore_index=True)
+
+    # print(gm_latest_invoice_rows_df)
+    # print(gm_latest_invoice_rows_df.loc[gm_latest_invoice_rows_df[s.column_part_number] == '7620320'])
+    return gm_latest_invoice_rows_df
+
